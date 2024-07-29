49cd0114b288ef3bb2342d3c09c4c3f824c00dac
antonio.garcia <ag19228@gmail.com>
2024-07-29 16:21:08 -0700

    new files

--- LAMBERT-GROUNDTRACKADJUSTMENT.py
+++ LAMBERT-GROUNDTRACKADJUSTMENT.py
@@ -78,7 +78,7 @@ def ExtendedLambert(t1, a_star, ecc_star, incl_star, ascending_node_star, arg_pe
     r2 = E.Earth_Radius + H_2
     lambda_max = np.sqrt((r1 + r2 - abs(r1 - r2)) / (r1 + r2 + abs(r1 - r2)))
 
-#Determining Keplerian Starters, lambda, x
+#Determining Keplerian Starters, lambda, x0
 # Step 1 - Setting a0 = a_star, ecc0 = ecc_star
     a0 = a_star
     ecc0 = ecc_star
@@ -118,72 +118,98 @@ def ExtendedLambert(t1, a_star, ecc_star, incl_star, ascending_node_star, arg_pe
     lambda0 = (np.sqrt(r1 * r2) * np.cos(theta_C0 / 2)) / s0
 
 # Determining the Allowable Number of Revolutions using modified Newton's Iteration
+#   Defining iteration methods
+    max_iterations = 100
+    tolerance = 1 * 10**-6
+
+    for i in range(max_iterations):
+
+        # Defining initial guess for x0
+        x0 = 0
+
+        # Calculating other values needed for the partial derivatives below
+        sigma = 1 - x0**2
+        y = np.sqrt(1 - lambda0**2 * (1 - x0**2))
+        s = (r1 + r2) / (1 + lambda0**2)
+        B_M = -1.5 * E.Earth_J2 * E.Earth_Radius**2 * (1.5 * np.sin(incl**2) - 1)
+        zeta = (1 + B_M * a0**-2 * B_e**(-3/2) * 0.5 * np.sqrt(E.Earth_mu) * tau_0)
+        cos_psi = (x0 * y) + (lambda0 * (1 - x0**2))
+        theta_ck = theta - (B_Omega * a0**(-7/2) * B_e**-2 * tau_0) + (2 * np.pi * K)
+
+        # Calculating Auxiliary Function, W
+        W = lambda0 - (np.sqrt(r1 * r2))**-1 * np.cos(((theta_C0 - (omeegadot_J2 * tau_0)) / 2) + (np.pi * K))
+
+        # Calculating partial derivatives
+        dLambda_F_dlambda = (sigma**-1 * y) - (lambda0**2 * y**-1) - (6 * np.sqrt(2) * s**(-3/2) * lambda0 * (1 +  lambda0**2)**-1 * zeta)
+        dLambda_upsilon_dlambda = -np.pi**-1 * sigma**(3/2) * dLambda_F_dlambda
+
+        dLambda_F_da = (2 * np.sqrt(2 * E.Earth_mu) * B_M * a0**-3 * B_e**(-3/2) * s(-3/2) * tau_0) + (7 * np.sqrt(2) * B_Omega * delta_l**-1 * a0**(-9/2) * B_e**-2 * s(-3/2) * tau_0 * zeta)
+        dLambda_upsilon_da = -np.pi**-1 * sigma**(3/2) * dLambda_F_da
+
+        da_dlambda = -2 * a0 * lambda0 * (1 + lambda0**2)**-1
+
+        dLambda_F_dB_e = (1.5 * np.sqrt(2 * E.Earth_mu) * B_M * a0**(-2) * B_e**(5/2) * s(-3/2) * tau_0) + (4 * np.sqrt(2) * B_Omega * delta_l**-1 * a0**(-7/2) * B_e**-3 * s**(-3/2) * tau_0 * zeta)
+        dLambda_Upsilon_dB_e = -np.pi**-1 * sigma**(3/2) * dLambda_F_dB_e
+
+        dB_e_dlambda = (-8 * sigma * ((s**2 * lambda0**5 * sigma) + (lambda0**3 * ((s**2 * y**2) -
+                    (sigma * ((r1 * r2) - s**2)))) + (lambda0 * (y**2 * (s**2 - (2 * r1 * r2)) - ((r1 * r2 * sigma)))) -
+                    (r1 * r2 * x0 * y * (1 - lambda0**2)))) / (s**2 * (1 + lambda0**2) * (y - (lambda0 * x0))**3 * y)
+
+        dLambda_Upsilon_dpsi = -np.pi**-1
+
+        dpsi_dlambda = (((lambda0 * x0**2) - (x0 * y) - lambda0) * sigma**(1/2) * y**-1) / cos_psi
+
+        dLambda_Upsilon_dx = np.pi**-1 * ((-6 * np.sqrt(2) * s**(-3/2) * x0 * sigma**(1/2) * zeta) + ((1 - (lambda0**3 * x0 * y**-1)) * sigma**(1/2)) - ((x0 - (lambda0 * y)) * x0 * sigma**(-1/2)))
+
+        da_dx = 2 * a0 * x0 * sigma**-1
+
+        dB_e_d_x = -2* B_e * ((-lambda0 * y**-1) + (x0 * sigma**-1))
+
+        dPsi_dx = (((x0 - (2 * x0 * y**2)) + (((2 * lambda0 * x0**2) - lambda0) * y)) * sigma**(-1/2) * y**-1) / cos_psi
+
+        dLambda_W_dx = 0
+
+        dLambda_W_da = (7/4) * np.sqrt(r1 * r2) * B_omega * a0**(-9/2) * B_e**(-2) * s**(-1) * tau_0(1 + (B_Omega * a0**(-7/2) * B_e**(-2) * delta_l**(-1) * tau_0)) * np.sin(theta_ck / 2)
+
+        dLambda_W_d_B_e = np.sqrt(r1 * r2) * B_omega * a0**(-7/2) * B_e**(-3) * s**(-1) * tau_0(1 + (B_Omega * a0**(-7/2) * B_e**(-2) * delta_l**(-1) * tau_0)) * np.sin(theta_ck / 2)
+
+        dLambda_W_dlambda = (1 - lambda0**2 + (2 * lambda0 * W)) * (1 + lambda0**2)**-1
 
 
+    # Values needed to calculate dUpsilon_dx
+        dPsi_Upsilon_dlambda = dLambda_upsilon_dlambda + (dLambda_upsilon_da * da_dlambda) + (dLambda_Upsilon_dB_e * dB_e_dlambda) + (dLambda_Upsilon_dpsi * dpsi_dlambda)
+        dPsi_Upsilon_dx = dLambda_Upsilon_dx + (dLambda_upsilon_da * da_dx) + (dLambda_Upsilon_dB_e * dB_e_d_x) + (dLambda_Upsilon_dpsi * dPsi_dx)
+
+    #   Values needed to calculate dlambda_dx
+        dW_dx = dLambda_W_dx + (dLambda_W_da * da_dx) + (dLambda_W_d_B_e * dB_e_d_x)
+        dW_dlambda = dLambda_W_dlambda + (dLambda_W_da * da_dlambda) + (dLambda_W_d_B_e * dB_e_dlambda)
+        dlambda_dx = -dW_dx / dW_dlambda
+
+        dUpsilon_dx = (dPsi_Upsilon_dlambda * dlambda_dx) + dPsi_Upsilon_dx
+
+        jacobian = np.array([[dPsi_Upsilon_dlambda, dPsi_Upsilon_dx], [dW_dlambda, dW_dx]])
+        jacobian_inv = np.linalg.inv(jacobian)
+        jacobian_inv_ident = np.identity(jacobian_inv)
+
+    #   Damping Factor
+        rho_Upislonx = 0.5
+
+     #  Equation 60
+        lambdaplus1 = lambda0 - (rho_Upislonx * jacobian_inv_ident * dUpsilon_dx)
+        xplus1 = x0 - (rho_Upislonx * jacobian_inv_ident * W)
 
 
-    # Defining initial guess for x
-    x = 0
-
-    # Calculating other values needed for the partial derivatives below
-    sigma = 1 - x**2
-    y = np.sqrt(1 - lambda0**2 * (1 - x**2))
-    s = (r1 + r2) / (1 + lambda0**2)
-    B_M = -1.5 * E.Earth_J2 * E.Earth_Radius**2 * (1.5 * np.sin(incl**2) - 1)
-    zeta = (1 + B_M * a0**-2 * B_e**(-3/2) * 0.5 * np.sqrt(E.Earth_mu) * tau_0)
-    cos_psi = (x * y) + (lambda0 * (1 - x**2))
-    theta_ck = theta - (B_Omega * a0**(-7/2) * B_e**-2 * tau_0) + (2 * np.pi * K)
-
-    # Calculating Auxiliary Function, W
-    W = lambda0 - (np.sqrt(r1 * r2))**-1 * np.cos(((theta_C0 - (omeegadot_J2 * tau_0)) / 2) + (np.pi * K))
-
-    # Calculating partial derivatives
-    dLambda_F_dlambda = (sigma**-1 * y) - (lambda0**2 * y**-1) - (6 * np.sqrt(2) * s**(-3/2) * lambda0 * (1 +  lambda0**2)**-1 * zeta)
-    dLambda_upsilon_dlambda = -np.pi**-1 * sigma**(3/2) * dLambda_F_dlambda
-
-    dLambda_F_da = (2 * np.sqrt(2 * E.Earth_mu) * B_M * a0**-3 * B_e**(-3/2) * s(-3/2) * tau_0) + (7 * np.sqrt(2) * B_Omega * delta_l**-1 * a0**(-9/2) * B_e**-2 * s(-3/2) * tau_0 * zeta)
-    dLambda_upsilon_da = -np.pi**-1 * sigma**(3/2) * dLambda_F_da
-
-    da_dlambda = -2 * a0 * lambda0 * (1 + lambda0**2)**-1
-
-    dLambda_F_dB_e = (1.5 * np.sqrt(2 * E.Earth_mu) * B_M * a0**(-2) * B_e**(5/2) * s(-3/2) * tau_0) + (4 * np.sqrt(2) * B_Omega * delta_l**-1 * a0**(-7/2) * B_e**-3 * s**(-3/2) * tau_0 * zeta)
-    dLambda_Upsilon_dB_e = -np.pi**-1 * sigma**(3/2) * dLambda_F_dB_e
-
-    dB_e_dlambda = (-8 * sigma * ((s**2 * lambda0**5 * sigma) + (lambda0**3 * ((s**2 * y**2) -
-                (sigma * ((r1 * r2) - s**2)))) + (lambda0 * (y**2 * (s**2 - (2 * r1 * r2)) - ((r1 * r2 * sigma)))) -
-                (r1 * r2 * x * y * (1 - lambda0**2)))) / (s**2 * (1 + lambda0**2) * (y - (lambda0 * x))**3 * y)
-
-    dLambda_Upsilon_dpsi = -np.pi**-1
-
-    dpsi_dlambda = (((lambda0 * x**2) - (x * y) - lambda0) * sigma**(1/2) * y**-1) / cos_psi
-
-    dLambda_Upsilon_dx = np.pi**-1 * ((-6 * np.sqrt(2) * s**(-3/2) * x * sigma**(1/2) * zeta) + ((1 - (lambda0**3 * x * y**-1)) * sigma**(1/2)) - ((x - (lambda0 * y)) * x * sigma**(-1/2)))
-
-    da_dx = 2 * a0 * x * sigma**-1
-
-    dB_e_d_x = -2* B_e * ((-lambda0 * y**-1) + (x * sigma**-1))
-
-    dPsi_dx = (((x - (2 * x * y**2)) + (((2 * lambda0 * x**2) - lambda0) * y)) * sigma**(-1/2) * y**-1) / cos_psi
-
-    dLambda_W_dx = 0
-
-    dLambda_W_da = (7/4) * np.sqrt(r1 * r2) * B_omega * a0**(-9/2) * B_e**(-2) * s**(-1) * tau_0(1 + (B_Omega * a0**(-7/2) * B_e**(-2) * delta_l**(-1) * tau_0)) * np.sin(theta_ck / 2)
-
-    dLambda_W_d_B_e = np.sqrt(r1 * r2) * B_omega * a0**(-7/2) * B_e**(-3) * s**(-1) * tau_0(1 + (B_Omega * a0**(-7/2) * B_e**(-2) * delta_l**(-1) * tau_0)) * np.sin(theta_ck / 2)
-
-    dLambda_W_dlambda = (1 - lambda0**2 + (2 * lambda0 * W)) * (1 + lambda0**2)**-1
+        if abs(lambda0 - lambdaplus1) < tolerance and abs(x0 - xplus1) < tolerance:
+            lambda_star = lambdaplus1
+            x_star = xplus1
+            break
+        lambda0 = lambdaplus1
+        x0 = xplus1
 
 
-# Values needed to calculate dUpsilon_dx
-    dPsi_Upsilon_dlambda = dLambda_upsilon_dlambda + (dLambda_upsilon_da * da_dlambda) + (dLambda_Upsilon_dB_e * dB_e_dlambda) + (dLambda_Upsilon_dpsi * dpsi_dlambda)
-    dPsi_Upsilon_dx = dLambda_Upsilon_dx + (dLambda_upsilon_da * da_dx) + (dLambda_Upsilon_dB_e * dB_e_d_x) + (dLambda_Upsilon_dpsi * dPsi_dx)
+    Nmaxtype1 =
 
-#   Values needed to calculate dlambda_dx
-    dW_dx = dLambda_W_dx + (dLambda_W_da * da_dx) + (dLambda_W_d_B_e * dB_e_d_x)
-    dW_dlambda = dLambda_W_dlambda + (dLambda_W_da * da_dlambda) + (dLambda_W_d_B_e * dB_e_dlambda)
-    dlambda_dx = -dW_dx / dW_dlambda
 
-    dUpsilon_dx = (dPsi_Upsilon_dlambda * dlambda_dx) + dPsi_Upsilon_dx
 
 
 
