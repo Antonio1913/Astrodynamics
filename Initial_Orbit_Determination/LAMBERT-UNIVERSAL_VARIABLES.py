1f5e1390589f71eece52c385e0265a76bf6ce3ce
antonio.garcia <ag19228@gmail.com>
2024-07-30 10:43:26 -0700

    new files

---
+++ Initial_Orbit_Determination/LAMBERT-UNIVERSAL_VARIABLES.py
@@ -0,0 +1,68 @@
+
+
+import numpy as np
+from SOLN_TO_KEPLER.FIndC2C3 import findc2c3
+
+def Lam_universe_var (r0_vec, r_vec, delta_t, t_m, mu):
+
+# Defining Position Magnitudes
+    r0_mag = np.linalg.norm(r0_vec)
+    r_mag = np.linalg.norm(r_vec)
+
+    cos_delta_v = np.dot(r0_vec, r_vec) / (r0_mag * r_mag)
+    delta_v =  np.acos(cos_delta_v)
+    sin_delta_v = t_m * np.sqrt(1 - (cos_delta_v*2))
+    A = t_m * np.sqrt(r_mag * r0_mag * (1 + cos_delta_v))
+
+    if A = 0:
+        raise ValueError("No Solution Available Given the Inputs.")
+
+    psi_n = 0
+    c2 = 1/2
+    c3 = 1/6
+    psi_up = 4 * (np.pi**2)
+    psi_low = -4 * np.pi
+
+    for i in range(100):
+
+        y_n = r0_mag + r_mag + (A * ((psi_n * c3) - 1) / np.sqrt(c2))
+
+        # if A and y_n are out of range
+        if A > 0 and y_n < 0.0:
+
+            # increasing psi low value
+            psi_low += np.pi
+
+            # Adjusting y_n value
+            y_n *= -1
+
+        chi_n = np.sqrt(y_n / c2)
+        delta_t_n = ((chi_n**3 * c3) + (A * np.sqrt(y_n))) / np.sqrt(mu)
+
+        if delta_t_n <= delta_t:
+            psi_low = psi_n
+        else:
+            psi_up = psi_n
+
+        psi_nplus1 = (psi_up + psi_low) / 2
+
+        c2, c3 = findc2c3(psi_nplus1)
+
+        psi_n = psi_nplus1
+
+        tolerance = 1 * 10 ** -6
+        if abs(delta_t_n - delta_t) < tolerance:
+
+            break
+
+    f = 1 - (y_n / r0_mag)
+    g_dot = 1 - (y_n / r_mag)
+    g = A * np.sqrt(y_n / mu)
+
+#   Defining Velocity Vectors
+    v0_vec = (r_vec - (f * r0_vec)) / g
+    v_vec = ((g_dot * r_vec) - r0_vec) / g
+
+    return v0_vec, v_vec
+
+
